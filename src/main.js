98858a78a80555d6732402e8eb7b089cfe8c4439
JohnyJoeJackassFuckbitch <54275464+JohnyJoeJackassFuckbitch@users.noreply.github.com>
2019-12-10 16:08:34 +0100

    dick

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
+const GameState = {
+  MAIN_MENU: new MainMenu(),
+  GAME: new Game(),
+}
+
+let game_state = GameState.MAIN_MENU;
+
 function setup() {
-  // put setup code here
+  createCanvas (800,800);
+  background (0);
+
+  game_state.setup();
 }
 
 function draw() {
-  // put drawing code here
+  game_state = game_state.draw();
 }
