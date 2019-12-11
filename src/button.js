b0bdbf1d9cfa25964a48d479c16b8a93c68de2f7
JohnyJoeJackassFuckbitch <54275464+JohnyJoeJackassFuckbitch@users.noreply.github.com>
2019-12-11 15:39:40 +0100

    button thing idk

--- src/button.js
+++ src/button.js
@@ -1,3 +1,15 @@
 class Button {
-    
+    position = new Vector(0, 0);
+    size = new Vector(0, 0);
+    text = "!!insert text!!";
+
+    draw() {
+        rect(
+            this.position.x, 
+            this.position.y, 
+            this.size.x,
+            this.size.y,
+        );
+    }
+
 }
